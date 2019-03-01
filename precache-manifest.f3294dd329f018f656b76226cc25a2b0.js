baab77132fa984216d8bdb31ba381fb69ccd26ce
Kyle Winckler <kyle.winckler@changehealthcare.com>
2019-02-28 16:32:46 -0800

    Updates

---
+++ precache-manifest.f3294dd329f018f656b76226cc25a2b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75518303bbf75bbd8476",
+    "url": "/static/css/main.8001ea7c.chunk.css"
+  },
+  {
+    "revision": "75518303bbf75bbd8476",
+    "url": "/static/js/main.75518303.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "97ca52f606179d2fb98afb0e6e7789d0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.a15ce670cd1c4f205c5f7e43bedfc55f.js"
+  "/precache-manifest.f3294dd329f018f656b76226cc25a2b0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/personal-website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
