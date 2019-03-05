04228f2ade38c5972779714c7d385f4f887f758b
Kyle Winckler <kyle.winckler@changehealthcare.com>
2019-03-05 14:27:23 -0800

    added text blurbs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h1 className="splash-text">WEB DEVELOPER.</h1>
+          <h3 className="blurb-one">"...this seasons must-hire developer!"</h3>
+          <h3 className="blurb-two">"my favorite son" - Kyle's Mom</h3>
+          <h3 className="blurb-three">"he's pretty cool..." - most people</h3>
         </div>
         <p>
           Kyle is super cool and you should hire him.
