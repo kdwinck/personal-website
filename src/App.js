ae20b3f61aa3293c31a54502cf71859bd8461b23
Kyle Winckler <kyle.winckler@changehealthcare.com>
2019-03-26 16:32:53 -0500

    add router and test nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router , Route, Link } from 'react-router-dom';
+
+import About from './containers/about';
+import Home from './containers/home';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,15 +11,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h1 className="splash-text">WEB DEVELOPER.</h1>
-          <h3 className="blurb-one">"...this seasons must-hire developer!"</h3>
-          <h3 className="blurb-two">"my favorite son" - Kyle's Mom</h3>
-          <h3 className="blurb-three">"he's pretty cool..." - most people</h3>
-        </div>
-        <p>
-          Kyle is super cool and you should hire him.
-        </p>
+        <Router>
+          <navbar>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/topics">Resume</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </navbar>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          {/* <Route path="/topics" component={Resume} /> */}
+          {/* <Route exact path="/" component={Projects} /> */}
+          {/* <Route path="/about" component={Contact} /> */}
+        </Router>
       </div>
     );
   }
---
+++ src/containers/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <h1 className="splash-text">WEB DEVELOPER.</h1>
+        <h3 className="blurb-one">"...this seasons must-hire developer!"</h3>
+        <h3 className="blurb-two">"my favorite son" - Kyle's Mom</h3>
+        <h3 className="blurb-three">"he's pretty cool..." - Josh</h3>
+      </div>
+    )
+  }
+}
+
+export default Home;
