bf1d8427b42b85e0855d6de9108d2dc63b121e18
Kyle Winckler <kyle.winckler@changehealthcare.com>
2019-03-27 17:12:06 -0500

    add button for disabling blurbs

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      movieModeEnabled: false,
+    };
+  }
+
+  toggleMovieMode= () => {
+    this.setState((state) => {
+      return { 
+        movieModeEnabled: !this.state.movieModeEnabled,
+      };
+    });
+  }
+
   render() {
     return (
       <div className="App-header">
         <h1 className="splash-text">WEB DEVELOPER.</h1>
-        <h3 className="blurb-one">"...this seasons must-hire developer!"</h3>
-        <h3 className="blurb-two">"my favorite son" - Kyle's Mom</h3>
-        <h3 className="blurb-three">"he's pretty cool..." - Josh</h3>
+        {
+          this.state.movieModeEnabled ? 
+            <div>
+              <h3 className="blurb-one">"...this seasons must-hire developer!"</h3>
+              <h3 className="blurb-two">"my favorite son" - Kyle's Mom</h3>
+              <h3 className="blurb-three">"he's pretty cool..." - Josh</h3>
+            </div>
+          : 
+            null
+        }
+        
+        {
+          this.state.movieModeEnabled ? 
+            <button onClick={this.toggleMovieMode} className="movieButton">Disable Movie Mode</button>
+          : 
+            <button onClick={this.toggleMovieMode} className="movieButton">Enable Movie Mode</button>
+        }
       </div>
     )
   }
